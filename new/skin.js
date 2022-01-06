63638aa3bd02c87f0e0bdc8dd136c1b1ced8e502
Ericke Gallardo <35298775+djrixg@users.noreply.github.com>
2022-01-06 16:59:05 +0800

    updated

--- new/skin.js
+++ new/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.13/18080
 // Filename: ustp.ggsk
-// Generated 2022-01-06T16:48:15
+// Generated 2022-01-06T16:56:50
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
