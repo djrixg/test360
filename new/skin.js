e74dc536ea379c202fd5ffb6f008b7347dcb7f05
Ericke Gallardo <35298775+djrixg@users.noreply.github.com>
2022-03-03 12:11:21 +0800

    removed tag

--- new/skin.js
+++ new/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.13/18080
 // Filename: ustp.ggsk
-// Generated 2022-03-03T12:08:36
+// Generated 2022-03-03T12:11:07
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
